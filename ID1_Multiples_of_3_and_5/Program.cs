96e737a68eaf071e543c363e8896d1f6713ba0b0
Seyone <seyone@mail.ru>
2015-10-02 13:08:53 +0300

    initial commit

---
+++ ID1_Multiples_of_3_and_5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ID1_Multiples_of_3_and_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lowerEndpoint = 0;
+            int upperEndpoint = 1000;
+            int sum = 0;
+
+            for (int i = lowerEndpoint; i < upperEndpoint; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine("Answer is {0}", sum);
+        }
+    }
+}
